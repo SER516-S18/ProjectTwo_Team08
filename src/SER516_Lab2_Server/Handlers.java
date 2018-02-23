039669bfb144acac524bb29a2ee8d4ad5192e00c
Vaishak Ramesh Vellore <vaishak.vellore@gmail.com>
2018-02-22 20:58:02 -0700

    handlers on server-side application to control GUI component which indicates server status

--- src/SER516_Lab2_Server/Handlers.java
+++ src/SER516_Lab2_Server/Handlers.java
@@ -6,25 +6,16 @@ import javax.swing.JPanel;
  * @author Vaishak Ramesh Vellore
  * @date 02/22/2018
  * @description Handlers on server-side application to control GUI component which indicates
- *  server status, console and control status for SER516/ProjectTwo_Team08.
+ *  server status for SER516/ProjectTwo_Team08.
+ *  
 */
 
 public class Handlers {
 
-    private static JPanel controlPanel;
     private static JPanel serverStatusPanel;
-    private static JPanel consolePanel;
-    
-
-    public static void setContolPanel(JPanel panel){
-        controlPanel = panel;
-    }
-
+   
     public static void setserverStatusPanel(JPanel panel){
     	serverStatusPanel = panel;
     }
 
-    public static void setConsolePanel(JPanel panel){
-        consolePanel = panel;
-    }
 }
