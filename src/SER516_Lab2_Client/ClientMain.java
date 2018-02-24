1e945744276475944d85495042f438f803d87d7a
esaifude <esaifude@asu.edu>
2018-02-23 23:21:46 -0700

    Running UI on main thread

--- src/SER516_Lab2_Client/ClientMain.java
+++ src/SER516_Lab2_Client/ClientMain.java
@@ -3,34 +3,29 @@ package SER516_Lab2_Client;
 
 import SER516_Lab2_Client.UIComponents.*;
 
-import java.awt.*;
 
 class ClientMain {
 
 
 
-	public static void initialiseGUI()
-	{
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ClientUI window = new ClientUI();
-					window.pack();
-					window.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-					String errorMessage = "No Client window set.";
-					Handlers.getInstance().displayConsoleMessage(errorMessage);
-				}
-			}
-		});
-	}
+	public static void initialiseGUI() {
+        try {
+            ClientUI window = new ClientUI();
+            window.pack();
+            window.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            String errorMessage = "No Client window set.";
+            Handlers.getInstance().displayConsoleMessage(errorMessage);
+        }
+
+    }
 
 	public static void main(String[] args){
 
 		initialiseGUI();
-		String message = "This client is running on Port: \" + Consts.PORT_NUMBER + \".";
-		Handlers.displayConsoleMessage(message);
+		String message = "This client is running on Port: " + Consts.PORT_NUMBER + ".";
+		Handlers.getInstance().displayConsoleMessage(message);
 	}
 
 
